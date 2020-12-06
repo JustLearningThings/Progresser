83e465864e04e72c7295b0b0047f50ebe701f7eb
Denny06 <denismacio@outlook.com>
2020-12-06 21:31:49 +0200

    fixed progress bar bug

--- client/src/components/dashboard/skill/DashboardSkill.js
+++ client/src/components/dashboard/skill/DashboardSkill.js
@@ -9,22 +9,18 @@ export default function DashboardSkill({ title, xp, requiredXp, description, id,
 
     return (
         <div className='skill' onClick={() => {
-            // history.push(`/dashboard/skills/${id}`, { title, level, xp, requiredXp, description, actions, date, id })
             history.push(`/dashboard/skills/${id}`, { id })
             }}>
-            {/* <h3 className='skill-title'>Skill 1</h3> */}
             <h3 className='skill-title'>{ title }</h3>
             <p className='skill-progress-bar-xp'>XP: {xp}/{requiredXp}</p>
             <div className='skill-progress-bar'>
                 <div
                     className='skill-progress-bar-inner'
-                    style={{ width: progressBarWidth }}
+                    style={{ width: `${progressBarWidth}%` }}
                 ></div>
             </div>
-            {/* this one may be optional */}
             <p className='skill-description'>
                 {description}
-                {/* Your skill description here.It should be concise and clear.We recommend to have a motivational word at the end. */}
             </p>
         </div>
     )

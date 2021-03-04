040baf29eb8b7bf606a0ecb6dc7b864dd58ea271
Denny06 <denismacio@outlook.com>
2021-03-04 15:41:55 +0200

    error modal redesign

--- client/src/components/form/modalError.js
+++ client/src/components/form/modalError.js
@@ -5,7 +5,7 @@ export default function ModalError({ message, handleExit }) {
     return (
         <div id='error-modal' onClick={() => handleExit()}>
             <div id='error-modal-box'>
-                <h4>Error</h4>
+                <h3>Error</h3>
                 <p>{message}</p>
             </div>
         </div>
